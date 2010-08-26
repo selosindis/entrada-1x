bef73ff0b88bba4bfd67a15490533d47f2c5ce91
Matt Simpson <matt.simpson@queensu.ca>
2010-08-26 11:03:08 -0400

    Added the new default config entries to the distribution config file.

--- www-root/core/config/config.inc.php
+++ www-root/core/config/config.inc.php
@@ -4,19 +4,20 @@ return array (
   'entrada_relative' => '/entrada',
   'entrada_absolute' => '/Users/username/Sites/entrada',
   'entrada_storage' => '/Users/username/Sites/entrada/core/storage',
-  'database' =>
-  array (
+  'database' => array (
+    'adapter' => 'mysql',
     'host' => 'localhost',
     'username' => 'entrada',
     'password' => '',
     'entrada_database' => 'entrada',
     'auth_database' => 'entrada_auth',
-    'clerkship_database' => 'entrada_clerkship',
+    'clerkship_database' => 'entrada_clerkship'
   ),
-  'admin' =>
-  array (
+  'admin' => array (
     'firstname' => 'System',
     'lastname' => 'Administrator',
-    'email' => 'null@entrada-project.org',
+    'email' => 'null@entrada-project.org'
   ),
+  'auth_username' => '',
+  'auth_password' => ''
 );

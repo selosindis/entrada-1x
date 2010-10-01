33976aeb1b77ce3fbe06dd7872a022843b25a661
Jonathan Fingland <jonathan.fingland@queensu.ca>
2010-10-01 15:56:07 -0400

    completes ticket #174

--- www-root/core/modules/public/tasks/index.inc.php
+++ www-root/core/modules/public/tasks/index.inc.php
@@ -26,6 +26,7 @@ if ((!defined("PARENT_INCLUDED")) || (!defined("IN_TASKS"))) {
 } else {
 
 	require_once("Models/tasks/Tasks.class.php");
+	require_once("Models/tasks/TaskCompletions.class.php");
 	$user = User::get($PROXY_ID);
 	
 	$sort_by = 'deadline';
@@ -40,49 +41,76 @@ if ((!defined("PARENT_INCLUDED")) || (!defined("IN_TASKS"))) {
 	$_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] = $sort_by;
 	$_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"] = $sort_order;
 	
-	$tasks = Tasks::getByRecipient($user,array('order_by' => $sort_by, 'dir' => $sort_order/*, 'limit' => 25, 'offset'=>0*/ )); //no limit for now. TODO work on pagination later.
+	//$tasks = Tasks::getByRecipient($user,array('order_by' => $sort_by, 'dir' => $sort_order/*, 'limit' => 25, 'offset'=>0*/ )); //no limit for now. TODO work on pagination later.
+	$task_verifications = TaskCompletions::getByVerifier($user->getID(), array("where" => "`verified_date` IS NULL" ));
+	$has_verification_requests = (count($task_verifications) > 0);
+	$task_completions = TaskCompletions::getByRecipient($user, array('order_by'=>array(array($sort_by, $sort_order))));
 	
 	?>
 	
 	<h1>My Tasks</h1>
 	
+	<?php if ($has_verification_requests) {?>
+	<div class="display-notice">You have outstanding task verification requests. Please go to the <a href="<?php echo ENTRADA_URL;?>/tasks?section=verification">Task Verification</a> page to manage them.</div>
+	<?php } ?>
 	<!--  Include something similar to learning event calendar/range select here -->
 	<table class="tableList" cellspacing="0" cellpadding="1" summary="List of Events">
 		<colgroup>
-			<col class="modified" />
+			<col class="status" width="3%" />
 			<col class="deadline" />
 			<col class="course" />
 			<col class="title" />
-			<col class="attachment" />
 		</colgroup>
 		<thead>
 			<tr>
-				<td class="modified">&nbsp;</td>
-				<td class="deadline<?php echo (($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] == "deadline") ? " sorted".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]) : ""); ?>"><?php echo admin_order_link("deadline", "Deadline"); ?></td>
+				<td>&nbsp;</td>
+				<td class="deadline<?php echo (($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] == "deadline") ? " sorted".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]) : ""); ?>"><?php echo public_order_link("deadline", "Deadline"); ?></td>
 				<td class="course">Course</td>
-				<td class="title<?php echo (($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] == "title") ? " sorted".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]) : ""); ?>"><?php echo admin_order_link("title", " Task Title"); ?></td>
-				<td class="attachment">&nbsp;</td>
+				<td class="title<?php echo (($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] == "title") ? " sorted".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]) : ""); ?>"><?php echo public_order_link("title", " Task Title"); ?></td>
 			</tr>
 		</thead>
 		<tbody>
-		<?php foreach ($tasks as $task) { ?>
+		<?php foreach ($task_completions as $task_completion) { 
+			$task = $task_completion->getTask();
+			?>
+		
 			<tr>
-				<td>&nbsp;</td>
-				<td><?php echo ($task->getDeadline()) ? date(DEFAULT_DATE_FORMAT,$task->getDeadline()) : ""; ?></td>
+				<td>
+					<?php
+					if ($task_completion->isCompleted()) { 
+						if ($task->isVerificationRequired()) {
+							if ($task_completion->isVerified()) {
+								?>
+								<img src="<?php echo ENTRADA_URL?>/images/task_completed.png" alt="Task Completed" title="Task Completed" />
+								<?php
+							} else {
+								?>
+								<img src="<?php echo ENTRADA_URL?>/images/task_pending.png" alt="Pending Verification" title="Pending Verification" />
+								<?php
+							}
+						} else {
+							?>
+							<img src="<?php echo ENTRADA_URL?>/images/task_completed.png" alt="Task Completed" title="Task Completed" />
+							<?php
+						}
+					} else { 
+						echo "&nbsp;"; 
+					}
+					?> 
+				</td>
+				<td><a href="<?php echo ENTRADA_URL; ?>/tasks?section=details&id=<?php echo $task->getID(); ?>"><?php echo ($task->getDeadline()) ? date(DEFAULT_DATE_FORMAT,$task->getDeadline()) : ""; ?></a></td>
 				<td><?php 
 					$course = $task->getCourse();
 					if ($course) {
-						?><a href="<?php echo ENTRADA_URL; ?>/courses?id=<?php echo $course->getID(); ?>">
+						?><a href="<?php echo ENTRADA_URL; ?>/tasks?section=details&id=<?php echo $task->getID(); ?>">
 						<?php echo $course->getTitle(); ?></a>
 					<?php
 					}
 				?></td>
 				<td><a href="<?php echo ENTRADA_URL; ?>/tasks?section=details&id=<?php echo $task->getID(); ?>"><?php echo $task->getTitle(); ?></a></td>
-				<td>&nbsp;</td>
 			</tr>
 		<?php } ?>
 		</tbody>	
 	</table>
 	<?php
-
 }

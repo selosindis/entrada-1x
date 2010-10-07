977dd1852e5785e876ed15ab42f99682e1ea0b91
Andrew Dos-Santos <andrew@medtech-215b.local>
2010-10-07 14:28:33 -0400

    Closed #136. Added admin reporting module to the Annual Reporting Module.

---
+++ www-root/core/modules/admin/annualreport/index.inc.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Entrada [ http://www.entrada-project.org ]
+ *
+ * Entrada is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Entrada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Entrada.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file is used to display annual report reports.
+ *
+ * @author Organisation: Queen's University
+ * @author Unit: School of Medicine
+ * @author Developer: Andrew Dos-Santos <andrew.dos-santos@queensu.ca>
+ * @copyright Copyright 2010 Queen's University. All Rights Reserved.
+ *
+*/
+
+if((!defined("PARENT_INCLUDED")) || (!defined("IN_ANNUAL_REPORT"))) {
+	exit;
+} elseif((!isset($_SESSION["isAuthorized"])) || (!$_SESSION["isAuthorized"])) {
+	header("Location: ".ENTRADA_URL);
+	exit;
+} elseif(!$ENTRADA_ACL->amIAllowed('annualreportadmin', 'read', false)) {
+	$ONLOAD[]	= "setTimeout('window.location=\\'".ENTRADA_URL."/admin/".$MODULE."\\'', 15000)";
+
+	$ERROR++;
+	$ERRORSTR[]	= "Your account does not have the permissions required to use this feature of this module.<br /><br />If you believe you are receiving this message in error please contact <a href=\"mailto:".html_encode($AGENT_CONTACTS["administrator"]["email"])."\">".html_encode($AGENT_CONTACTS["administrator"]["name"])."</a> for assistance.";
+
+	echo display_error();
+
+	application_log("error", "Group [".$_SESSION["permissions"][$_SESSION[APPLICATION_IDENTIFIER]["tmp"]["proxy_id"]]["group"]."] and role [".$_SESSION["permissions"][$_SESSION[APPLICATION_IDENTIFIER]["tmp"]["proxy_id"]]["role"]."] does not have access to this module [".$MODULE."]");
+} else {
+	?>
+	<style type="text/css">
+		ol.system-reports li {
+			width:			70%;
+			color:			#666666;
+			font-size:		12px;
+			padding:		0px 15px 15px 0px;
+			margin-left:	5px;
+		}
+		
+		ol.system-reports li a {
+			font-size:		13px;
+			font-weight:	bold;
+		}
+	</style>
+	<h1><?php echo $MODULES[strtolower($MODULE)]["title"]; ?></h1>
+	
+	<h2 style="color: #669900">Completion Reports</h2>
+	<ol class="system-reports">
+		<li>
+			<a href="<?php echo ENTRADA_URL; ?>/admin/annualreport?section=report-status">Report By Faculty Member</a><br />
+			A report idicating a faculty Member's annual report completiong status for a given period.
+		</li>
+	</ol>
+	<?php
+}
+?>

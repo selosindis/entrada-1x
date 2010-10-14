dddadc6922793200844a6b233d490d7045cedd05
Jonathan Fingland <jonathan.fingland@queensu.ca>
2010-10-14 15:17:26 -0400

    adds Template and TemplateMailer classes for handing template processing and mailiing of template results respectively.

---
+++ www-root/core/library/Models/utility/Template.class.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Entrada [ http://www.entrada-project.org ]
+ * 
+ *
+ * 
+ * @author Organisation: Queen's University
+ * @author Unit: School of Medicine
+ * @author Developer: Jonathan Fingland <jonathan.fingland@queensu.ca>
+ * @copyright Copyright 2010 Queen's University. All Rights Reserved.
+*/
+
+class Template {
+	
+	/**
+	 * 
+	 * @var DOMDocument
+	 */
+	private $_template_data;
+	
+	function __construct($filename="") {
+		if ($filename) {
+			$this->loadFile($filename);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param string $filename file with xml template data
+	 */
+	public function loadFile($filename) {
+		$filename = realpath($filename);
+		if (file_exists($filename)) {
+			$this->_template_data = new DOMDocument();
+			$this->_template_data->load($filename);
+		} else {
+			throw new RuntimeException("File not found");
+		} 
+	}
+	
+	public function getResult($language="", array $bind_array = array()) {
+		if (!$language) {
+			if (defined("DEFAULT_LANGUAGE")) {
+				$language = DEFAULT_LANGUAGE;
+			} else {
+				throw new RuntimeException("Invalid language provided and no default language set.");
+			}
+		}
+		
+		if ($this->_template_data) {
+			$xpath = new DOMXpath($this->_template_data);
+			$t_query = "//template[@lang='".$language."']";
+			$t_data = $xpath->query($t_query);
+			
+			//if the selected language is not available, try the default
+			if (($t_data->length == 0) && (defined("DEFAULT_LANGUAGE"))) {
+				$language = DEFAULT_LANGUAGE;
+				$t_query = "//template[@lang='".$language."']";
+				$t_data = $xpath->query($t_query);
+			}
+			
+			//only take the first result, if any
+			if ($t_data->length > 0) {
+				$template = $t_data->item(0);
+				$working_template = $template->cloneNode(true);
+				//$this->_template_data->documentElement->appendChild($working_template);
+				//now get every descendent and process with bound values
+				$n_query = "./descendant-or-self::text()";
+				$nodes = $xpath->query($n_query, $working_template);
+				if ($nodes->length > 0) {
+					for($i=0,$len = $nodes->length; $i<$len;$i++) {
+						$node = $nodes->item($i);
+						$text = $node->wholeText;
+						$text_len = strlen($text);
+						foreach ($bind_array as $key=>$value) {
+							$text = str_ireplace("%".strtoupper($key)."%",$value,$text);
+						}
+						$node->replaceData(0,$text_len,$text);
+					}
+					
+				}
+				$sxl = simplexml_import_dom($working_template);
+				return $sxl;
+				
+			}
+
+		}
+		throw new Exception("Template not loaded.");
+	}	
+}

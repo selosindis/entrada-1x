837ae0a91c4452b2959a1c7220675ae346223093
Don Zuiker (MEdTech Unit) <zuikerd@s64.n105.queensu.ca>
2011-04-06 12:53:51 -0400

    Added insert of mtdtracking module.

--- www-root/setup/upgrade/1201/entrada.sql
+++ www-root/setup/upgrade/1201/entrada.sql
@@ -1,3 +1,6 @@
+INSERT INTO `communities_modules` (`module_id`, `module_shortname`, `module_version`, `module_title`, `module_description`, `module_active`, `module_permissions`, `updated_date`, `updated_by`) VALUES
+(8, 'mtdtracking', '1.0.0', 'MTD Tracking', 'The MTD Tracking module allows Program Assistants to enter the weekly schedule for each of their Residents.', 0, 'a:2:{s:4:"edit";i:1;s:5:"index";i:0;}', 1216256830, 5440);
+
 ALTER TABLE `communities_modules` ADD COLUMN `module_visible` int(1) NOT NULL DEFAULT '1' AFTER `module_active`;
 UPDATE `communities_modules` SET `module_visible` = '0' WHERE `module_shortname` = 'mtdtracking';
 

34ae9ffa621331ed0d7ad62311bc43251f699a9f
Don Zuiker <don.zuiker@queensu.ca>
2011-06-03 14:08:25 -0400

    Added PGME Evaluation Response database tables.
    Signed-off-by: Don Zuiker <don.zuiker@queensu.ca>

--- www-root/setup/install/sql/entrada.sql
+++ www-root/setup/install/sql/entrada.sql
@@ -3362,7 +3362,7 @@ CREATE TABLE IF NOT EXISTS `settings` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 INSERT INTO `settings` (`shortname`, `value`) VALUES
-('version_db', '1203'),
+('version_db', '1204'),
 ('version_entrada', '1.2.0');
 
 CREATE TABLE `small_groups` (
@@ -3839,3 +3839,19 @@ CREATE TABLE IF NOT EXISTS `group_members` (
   KEY `member_active` (`member_active`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
+CREATE TABLE `pg_eval_response_rates` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `program_name` varchar(100) NOT NULL,
+  `response_type` varchar(20) NOT NULL,
+  `completed` int(10) NOT NULL,
+  `distributed` int(10) NOT NULL,
+  `percent_complete` int(3) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `pg_one45_community` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `one45_name` varchar(50) NOT NULL,
+  `community_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;

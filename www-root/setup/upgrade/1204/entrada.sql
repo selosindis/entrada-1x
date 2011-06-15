ea625fa8f5cb56cf60e687c50c83a9af18e01f1b
Brandon Thorn <brandon.thorn@queensu.ca>
2011-06-15 15:56:15 -0400

    Changed the update sql
    Signed-off-by: Brandon Thorn <brandon.thorn@queensu.ca>

--- www-root/setup/upgrade/1204/entrada.sql
+++ www-root/setup/upgrade/1204/entrada.sql
@@ -18,21 +18,4 @@ CREATE TABLE `pg_one45_community` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `objective_organisation`(
-`objective_id` INT(12) NOT NULL, 
-`organisation_id` INT(12) NOT NULL, 
-KEY `objective_id` (`objective_id`),
-KEY `organistion_id` (`organisation_id`)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `topic_organisation`(
-`topic_id` INT(12) NOT NULL, 
-`organisation_id` INT(12) NOT NULL, 
-KEY `topic_id` (`topic_id`),
-KEY `organisation_id` (`organisation_id`)
-) ENGINE = MyISAM DEFAULT CHARSET=utf8;
-
-INSERT INTO `objective_organisation` SELECT a.`objective_id`, b.`organisation_id` FROM `global_lu_objectives` AS a JOIN `entrada_auth.organisations` AS b ON 1=1;
-
-ALTER TABLE `event_contacts` ADD `contact_role` ENUM('teacher','tutor','ta','auditor') NOT NULL AFTER `proxy_id`;
 

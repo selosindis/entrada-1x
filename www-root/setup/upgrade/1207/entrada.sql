9afefaeefdbae5d8edd7a50bee3ddc72f271c5c9
James Ellis <james.ellis@queensu.ca>
2011-07-15 16:08:48 -0400

    Added missing change in the database upgrade script from 1206 to 1207.

--- www-root/setup/upgrade/1207/entrada.sql
+++ www-root/setup/upgrade/1207/entrada.sql
@@ -1,5 +1,6 @@
 UPDATE `settings` SET `value` = '1207' WHERE `shortname` = 'version_db';
 
+ALTER TABLE `events` ADD `parent_id` int(11) NOT NULL AFTER `event_id`;
 ALTER TABLE `events` ADD `event_children` int(11) NOT NULL AFTER `parent_id`;
 ALTER TABLE `events` ADD `include_parent_description` tinyint(1) NOT NULL DEFAULT 1 AFTER `event_description`;
 ALTER TABLE `events` ADD `include_parent_message` int(11) NOT NULL DEFAULT 1 AFTER `event_message`;

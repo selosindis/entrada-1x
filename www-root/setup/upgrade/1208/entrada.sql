06d871e719b1db0873505ed31aba7dbd6b3e2c28
Matt Simpson <matt.simpson@queensu.ca>
2011-07-22 13:43:24 -0400

    Fixed issues with the upgrade SQL files.

--- www-root/setup/upgrade/1208/entrada.sql
+++ www-root/setup/upgrade/1208/entrada.sql
@@ -1,6 +1,4 @@
-UPDATE `settings` SET `value` = '1208' WHERE `shortname` = 'version_db';
-
-CREATE TABLE `course_audience`(
+CREATE TABLE IF NOT EXISTS `course_audience`(
 	`caudience_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	`course_id` INT NOT NULL, 
 	`audience_type` ENUM('proxy_id','group_id') NOT NULL, 
@@ -8,13 +6,13 @@ CREATE TABLE `course_audience`(
 	`enroll_start` BIGINT NOT NULL, 
 	`enroll_finish` BIGINT NOT NULL, 
 	`audience_active` INT(1) NOT NULL DEFAULT '1', 
- KEY `event_id` (`event_id`), 
- KEY `audience_type` (`audience_type`), 
- KEY `audience_value` (`audience_value`), 
- KEY `audience_active` (`audience_active`)
+	KEY `event_id` (`event_id`), 
+	KEY `audience_type` (`audience_type`), 
+	KEY `audience_value` (`audience_value`), 
+	KEY `audience_active` (`audience_active`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `group_organisation`(
+CREATE TABLE IF NOT EXISTS `group_organisation`(
 	`group_id` INT NOT NULL, 
 	`organisation_id` INT NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET = utf8;
@@ -22,3 +20,5 @@ CREATE TABLE `group_organisation`(
 ALTER TABLE `event_audience` MODIFY `audience_type` ENUM('proxy_id','grad_year','organisation_id','course_id');
 ALTER TABLE `courses` ADD COLUMN `permission` ENUM('open','closed') NOT NULL DEFAULT 'closed' AFTER `course_message`;
 ALTER TABLE `groups` ADD COLUMN `parent_id` INT DEFAULT NULL AFTER `group_name`;
+
+UPDATE `settings` SET `value` = '1208' WHERE `shortname` = 'version_db';

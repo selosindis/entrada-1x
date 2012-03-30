2bff91e326aff902f4a40d9b63540b9f7462ff78
Ryan Warner <ryan.warner@queensu.ca>
2012-03-30 10:15:45 -0400

    closes #589: feed url changed

--- www-root/templates/default/languages/en.lang.php
+++ www-root/templates/default/languages/en.lang.php
@@ -120,7 +120,7 @@ return array (
     "public_dashboard_feeds" => array (
 		"global" => array (
 			array ("title" => "Entrada Announcement Feed", "url" => "http://www.entrada-project.org/news/feed", "removable" => false),
-			array ("title" => "Zend DevZone", "url" => "http://devzone.zend.com/tag/Zend_Framework_Management/format/rss2.0", "removable" => false),
+			array ("title" => "Zend DevZone", "url" => "http://feeds.feedburner.com/PHPDevZone", "removable" => false),
 			array ("title" => "Insider Medicine", "url" => "http://insidermedicine.ca/xml/Patient/insidermedicine_English.xml", "removable" => false),
 			array ("title" => "Google News Top Stories", "url" => "http://news.google.com/news?pz=1&cf=all&ned=ca&hl=en&topic=h&num=3&output=rss", "removable" => false)
 		),

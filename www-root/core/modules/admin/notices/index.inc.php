7ba7b6987d0f5df81802b4ec27ca1cbeda435e73
Matt Simpson <simpson@queensu.ca>
2012-04-11 16:33:19 -0600

    refs #614 Fixed the SQL so the notices show up on the index page. Also tidied up the interface a bit.
    Signed-off-by: Matt Simpson <simpson@queensu.ca>

--- www-root/core/modules/admin/notices/index.inc.php
+++ www-root/core/modules/admin/notices/index.inc.php
@@ -22,56 +22,41 @@
  *
 */
 
-if((!defined("PARENT_INCLUDED")) || (!defined("IN_NOTICES"))) {
+if ((!defined("PARENT_INCLUDED")) || (!defined("IN_NOTICES"))) {
 	exit;
-} elseif((!isset($_SESSION["isAuthorized"])) || (!$_SESSION["isAuthorized"])) {
+} elseif ((!isset($_SESSION["isAuthorized"])) || (!$_SESSION["isAuthorized"])) {
 	header("Location: ".ENTRADA_URL);
 	exit;
-} elseif(!$ENTRADA_ACL->amIAllowed('notice', 'update', false)) {
+} elseif (!$ENTRADA_ACL->amIAllowed("notice", "update", false)) {
 	$ONLOAD[]	= "setTimeout('window.location=\\'".ENTRADA_URL."/admin/".$MODULE."\\'', 15000)";
 
-	$ERROR++;
-	$ERRORSTR[]	= "Your account does not have the permissions required to use this feature of this module.<br /><br />If you believe you are receiving this message in error please contact <a href=\"mailto:".html_encode($AGENT_CONTACTS["administrator"]["email"])."\">".html_encode($AGENT_CONTACTS["administrator"]["name"])."</a> for assistance.";
+	add_error("Your account does not have the permissions required to use this feature of this module.<br /><br />If you believe you are receiving this message in error please contact <a href=\"mailto:".html_encode($AGENT_CONTACTS["administrator"]["email"])."\">".html_encode($AGENT_CONTACTS["administrator"]["name"])."</a> for assistance.");
 
 	echo display_error();
 
 	application_log("error", "Group [".$_SESSION["permissions"][$_SESSION[APPLICATION_IDENTIFIER]["tmp"]["proxy_id"]]["group"]."] and role [".$_SESSION["permissions"][$_SESSION[APPLICATION_IDENTIFIER]["tmp"]["proxy_id"]]["role"]."] does not have access to this module [".$MODULE."]");
 } else {
-		
-	if (isset($organisation_list) && $organisation_list && count($organisation_list) > 1) {
-		$sidebar_html = "<ul class=\"menu\">\n";
-		foreach ($organisation_list as $key => $organisation_title) {
-			if ($key == $ORGANISATION_ID) {
-				$sidebar_html .= "<li class=\"on\"><a href=\"".ENTRADA_URL."/admin/notices?".replace_query(array("org" => $key))."\">".html_encode($organisation_title)."</a></li>\n";
-			} else {
-				$sidebar_html .= "<li class=\"off\"><a href=\"".ENTRADA_URL."/admin/notices?".replace_query(array("org" => $key))."\">".html_encode($organisation_title)."</a></li>\n";
-			}
-		}
-		$sidebar_html .= "</ul>\n";
-
-		new_sidebar_item("Organisations", $sidebar_html, "display-style", "open");
-	}
 	/**
 	 * This query will automatically remove notices that have expired more than 5 days ago.
 	 */
-	if((defined("DEVELOPMENT_MODE")) && (DEVELOPMENT_MODE == false)) {
+	if ((defined("DEVELOPMENT_MODE")) && (DEVELOPMENT_MODE == false)) {
 		$query = "DELETE FROM `notices` WHERE `display_until` <= '".strtotime("-5 days", strtotime("00:00:00"))."'";
 		$db->Execute($query);
 	}
-	
+
 	/**
 	 * Update requested column to sort by.
 	 * Valid: date, teacher, title, phase
 	 */
-	if(isset($_GET["sb"])) {
-		if(@in_array(trim($_GET["sb"]), array("date" , "summary", "target", "organisation"))) {
-			$_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"]	= trim($_GET["sb"]);
+	if (isset($_GET["sb"])) {
+		if (@in_array(trim($_GET["sb"]), array("date", "summary"))) {
+			$_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] = trim($_GET["sb"]);
 		}
 
-		$_SERVER["QUERY_STRING"]	= replace_query(array("sb" => false));
+		$_SERVER["QUERY_STRING"] = replace_query(array("sb" => false));
 	} else {
-		if(!isset($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"])) {
-			$_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] = "target";
+		if (!isset($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"])) {
+			$_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] = "date";
 		}
 	}
 
@@ -79,13 +64,13 @@ if((!defined("PARENT_INCLUDED")) || (!defined("IN_NOTICES"))) {
 	 * Update requested order to sort by.
 	 * Valid: asc, desc
 	 */
-	if(isset($_GET["so"])) {
+	if (isset($_GET["so"])) {
 		$_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"] = ((strtolower($_GET["so"]) == "desc") ? "desc" : "asc");
 
-		$_SERVER["QUERY_STRING"]	= replace_query(array("so" => false));
+		$_SERVER["QUERY_STRING"] = replace_query(array("so" => false));
 	} else {
-		if(!isset($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"])) {
-			$_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"] = "asc";
+		if (!isset($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"])) {
+			$_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"] = "desc";
 		}
 	}
 
@@ -93,20 +78,17 @@ if((!defined("PARENT_INCLUDED")) || (!defined("IN_NOTICES"))) {
 	 * Check if preferences need to be updated on the server at this point.
 	 */
 	preferences_update($MODULE, $PREFERENCES);
-	
+
 	/**
 	 * Provide the queries with the columns to order by.
 	 */
 	switch($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"]) {
-		case "date" :
-			$SORT_BY	= "`notices`.`display_until` ".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]);
-		break;
 		case "summary" :
-			$SORT_BY	= "`notices`.`notice_summary` ".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]).", `notices`.`display_until` ASC";
+			$SORT_BY = "`notices`.`notice_summary` ".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]).", `notices`.`display_until` ASC";
 		break;
-		case "target" :
+		case "date" :
 		default :
-			$SORT_BY	= "`notices`.`target` ".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]).", `notices`.`display_until` ASC";
+			$SORT_BY = "`notices`.`display_until` ".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]);
 		break;
 	}
 	?>
@@ -115,67 +97,71 @@ if((!defined("PARENT_INCLUDED")) || (!defined("IN_NOTICES"))) {
 	<div class="display-generic">
 		These notices will be displayed to the user directly on their <?php echo APPLICATION_NAME; ?> dashboard as well as on a publicly accessible RSS feed. <strong>Please note</strong> we do not recommend posting confidential information inside these notices.
 	</div>
-	<?php if($ENTRADA_ACL->amIAllowed('notice', 'create', false)) { ?>
-	<div style="float: right">
-		<ul class="page-action">
-			<li><a href="<?php echo ENTRADA_URL; ?>/admin/<?php echo $MODULE; ?>?section=add" class="strong-green">Add New Notice</a></li>
-		</ul>
-	</div>
-	<?php } ?>
-	<div style="clear: both"></div>
-
 	<?php
-	$query	= "SELECT `notices`.*, `organisations`.`organisation_title` FROM `notices` LEFT JOIN `".AUTH_DATABASE."`.`organisations` ON `".AUTH_DATABASE."`.`organisations`.`organisation_id` = `notices`.`organisation_id` WHERE `notices`.`organisation_id` = ".$db->qstr($ORGANISATION_ID)." ORDER BY ".$SORT_BY;
-	$results	= ((USE_CACHE) ? $db->CacheGetAll(CACHE_TIMEOUT, $query) : $db->GetAll($query));
-	if($results) {
+	if ($ENTRADA_ACL->amIAllowed("notice", "create", false)) {
+		?>
+		<div style="float: right">
+			<ul class="page-action">
+				<li><a href="<?php echo ENTRADA_URL; ?>/admin/<?php echo $MODULE; ?>?section=add" class="strong-green">Add New Notice</a></li>
+			</ul>
+		</div>
+		<div style="clear: both"></div>
+		<?php
+	}
+
+	$query = "	SELECT `notices`.*, `organisations`.`organisation_title`
+				FROM `notices`
+				JOIN `".AUTH_DATABASE."`.`organisations`
+				ON `".AUTH_DATABASE."`.`organisations`.`organisation_id` = `notices`.`organisation_id`
+				WHERE `notices`.`organisation_id` = ".$db->qstr($ENTRADA_USER->getActiveOrganisation())."
+				ORDER BY ".$SORT_BY;
+	$results = $db->GetAll($query);
+	if ($results) {
 		?>
 		<form action="<?php echo ENTRADA_URL; ?>/admin/notices?section=delete" method="post">
-		<table class="tableList" cellspacing="0" summary="List of Notices">
-		<colgroup>
-			<col class="modified" />
-			<col class="general" />
-			<col class="title" />
-			<col class="date" />
-			<col class="accesses" />
-		</colgroup>
-		<thead>
-			<tr>
-				<td class="modified">&nbsp;</td>
-				<td class="general<?php echo (($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] == "organisation") ? " sorted".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]) : ""); ?>"><?php echo admin_order_link("organisation", "Target Organisation"); ?></td>
-				<td class="title<?php echo (($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] == "summary") ? " sorted".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]) : ""); ?>"><?php echo admin_order_link("summary", "Notice Summary"); ?></td>
-				<td class="date<?php echo (($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] == "date") ? " sorted".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]) : ""); ?>"><?php echo admin_order_link("date", "Display Until"); ?></td>
-				<td class="accesses">Reads</td>
-			</tr>
-		</thead>
-		<tfoot>
-			<tr>
-				<td></td>
-				<td colspan="4" style="padding-top: 10px">
-					<input type="submit" class="button" value="Delete Selected" />
-				</td>
-			</tr>
-		</tfoot>
-		<tbody>
-			<?php
-			foreach($results as $result) {
-				$url			= ENTRADA_URL."/admin/notices?section=edit&amp;id=".$result["notice_id"];
-				$expired		= false;
-				
-				if(($display_until = (int) $result["display_until"]) && ($display_until < time())) {
-					$expired	= true;
-				}
+			<table class="tableList" cellspacing="0" summary="List of Notices">
+				<colgroup>
+					<col class="modified" />
+					<col class="date" />
+					<col class="title" />
+					<col class="accesses" />
+				</colgroup>
+				<thead>
+					<tr>
+						<td class="modified">&nbsp;</td>
+						<td class="date<?php echo (($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] == "date") ? " sorted".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]) : ""); ?>"><?php echo admin_order_link("date", "Display Until"); ?></td>
+						<td class="title<?php echo (($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["sb"] == "summary") ? " sorted".strtoupper($_SESSION[APPLICATION_IDENTIFIER][$MODULE]["so"]) : ""); ?>"><?php echo admin_order_link("summary", "Notice"); ?></td>
+						<td class="accesses">Reads</td>
+					</tr>
+				</thead>
+				<tfoot>
+					<tr>
+						<td></td>
+						<td colspan="3" style="padding-top: 10px">
+							<input type="submit" class="button" value="Delete Selected" />
+						</td>
+					</tr>
+				</tfoot>
+				<tbody>
+					<?php
+					foreach ($results as $result) {
+						$url = ENTRADA_URL."/admin/notices?section=edit&amp;id=".$result["notice_id"];
+						$expired = false;
 
-				echo "<tr id=\"notice-".$result["notice_id"]."\" class=\"notice".(($expired) ? " na" : "")."\">\n";
-				echo "	<td class=\"modified\"><input type=\"checkbox\" name=\"delete[]\" value=\"".$result["notice_id"]."\" /></td>\n";
-				echo "	<td class=\"general\">".(($url) ? "<a href=\"".$url."\">" : "").html_encode($result["organisation_title"]).(($url) ? "</a>" : "")."</td>\n";
-				echo "	<td class=\"title\">".(($url) ? "<a href=\"".$url."\">" : "").html_encode($result["notice_summary"]).(($url) ? "</a>" : "")."</td>\n";
-				echo "	<td class=\"date\">".(($url) ? "<a href=\"".$url."\">" : "").date(DEFAULT_DATE_FORMAT, $result["display_until"]).(($url) ? "</a>" : "")."</td>\n";
-				echo "	<td class=\"accesses\">".count_notice_reads($result["notice_id"])."</td>\n";
-				echo "</tr>\n";
-			}
-			?>
-		</tbody>
-		</table>
+						if (($display_until = (int) $result["display_until"]) && ($display_until < time())) {
+							$expired = true;
+						}
+
+						echo "<tr id=\"notice-".$result["notice_id"]."\" class=\"notice".(($expired) ? " na" : "")."\">\n";
+						echo "	<td class=\"modified\"><input type=\"checkbox\" name=\"delete[]\" value=\"".$result["notice_id"]."\" /></td>\n";
+						echo "	<td class=\"date\">".(($url) ? "<a href=\"".$url."\">" : "").date(DEFAULT_DATE_FORMAT, $result["display_until"]).(($url) ? "</a>" : "")."</td>\n";
+						echo "	<td class=\"title content-small\">".limit_chars(strip_tags($result["notice_summary"]), 125, false, true)."</td>\n";
+						echo "	<td class=\"accesses\" style=\"text-align: center\">".count_notice_reads($result["notice_id"])."</td>\n";
+						echo "</tr>\n";
+					}
+					?>
+				</tbody>
+			</table>
 		</form>
 		<?php
 	} else {
@@ -187,4 +173,3 @@ if((!defined("PARENT_INCLUDED")) || (!defined("IN_NOTICES"))) {
 		<?php
 	}
 }
-?>

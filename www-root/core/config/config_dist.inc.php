84a16860440bfc509f3fc787b6389ddfb2f5f034
Matt Simpson <simpson@queensu.ca>
2012-04-11 17:18:19 -0600

    closes #618 Changed the default config_dist.inc.php file back the original.

--- www-root/core/config/config_dist.inc.php
+++ www-root/core/config/config_dist.inc.php
@@ -1,23 +1,23 @@
 <?php
 return array (
-  'entrada_url' => 'http://localhost/beta/entrada/www-root',
-  'entrada_relative' => '/beta/entrada/www-root',
-  'entrada_absolute' => '/usr/local/zend/apache2/htdocs/beta/entrada/www-root',
-  'entrada_storage' => '/usr/local/zend/apache2/htdocs/beta/entrada/www-root/core/storage',
+  'entrada_url' => 'http://localhost/entrada',
+  'entrada_relative' => '/entrada',
+  'entrada_absolute' => '/Users/username/Sites/entrada',
+  'entrada_storage' => '/Users/username/Sites/entrada/core/storage',
   'database' => array (
     'adapter' => 'mysql',
     'host' => 'localhost',
-    'username' => 'brandon',
-    'password' => 'MySQLAdmin.01',
-    'entrada_database' => 'beta_entrada',
-    'auth_database' => 'beta_entrada_auth',
-    'clerkship_database' => 'beta_entrada_clerkship',
+    'username' => 'entrada',
+    'password' => '',
+    'entrada_database' => 'entrada',
+    'auth_database' => 'entrada_auth',
+    'clerkship_database' => 'entrada_clerkship'
   ),
   'admin' => array (
-    'firstname' => 'Brandon',
-    'lastname' => 'Thorn',
-    'email' => 'bt37@queensu.ca',
+    'firstname' => 'System',
+    'lastname' => 'Administrator',
+    'email' => 'null@entrada-project.org'
   ),
-  'auth_username' => 'bfacb8709a42992f017faec36137bec7',
-  'auth_password' => '0920ccb6220895f2db6afbfa41e2a9a1'
+  'auth_username' => '',
+  'auth_password' => ''
 );

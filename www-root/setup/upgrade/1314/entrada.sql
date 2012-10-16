60c86072741f36f0416a3b1645a2805005c8c5fc
Matt Simpson <simpson@queensu.ca>
2012-10-16 16:43:33 -0400

    Updated the Entrada upgrade SQL files.

--- www-root/setup/upgrade/1314/entrada.sql
+++ www-root/setup/upgrade/1314/entrada.sql
@@ -2,4 +2,4 @@ ALTER TABLE `attached_quizzes` ADD COLUMN `require_attendance` INT(1) NOT NULL D
 ALTER TABLE `attached_quizzes` ADD COLUMN `random_order` INT(1) NOT NULL DEFAULT '0' AFTER `require_attendance`;
 ALTER TABLE `community_discussion_topics` ADD COLUMN `anonymous` INT(1) NOT NULL DEFAULT '0' AFTER `proxy_id`;
 
-UPDATE `settings` SET `db_version` = '1314';
+UPDATE `settings` SET `value` = '1314' WHERE `shortname` = 'version_db';

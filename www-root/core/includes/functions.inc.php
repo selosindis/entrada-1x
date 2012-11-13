1f0f1900c32f1452d5af1fcfbc62e81624f4238d
Brandon Thorn <brandon.thorn@queensu.ca>
2012-11-13 13:22:08 -0500

    Removed duplicate function from functions.inc.php

--- www-root/core/includes/functions.inc.php
+++ www-root/core/includes/functions.inc.php
@@ -15361,63 +15361,6 @@ function groups_get_explicitly_enrolled_course_ids($proxy_id = 0, $only_active_g
 }
 
 /**
- * This function returns the courses that the proxy id is explicitely enrolled in
- *
- * @param int $proxy_id
- * @return array $group
- */
-function groups_get_explicitly_enrolled_course_ids($proxy_id = 0, $only_active_groups = false) {
-	global $db, $ENTRADA_USER;
-	
-	$proxy_id = (int) $proxy_id;
-	$only_active_groups = (bool) $only_active_groups;
-	
-	$course_ids = array();
-	
-	if ($proxy_id) {
-				$query = "	SELECT a.course_id FROM courses AS a
-					LEFT JOIN course_audience AS b
-					ON a.course_id = b.course_id
-					WHERE a.`organisation_id` = ".$db->qstr($ENTRADA_USER->getActiveOrganisation())."
-					AND (
-						(
-							(
-								audience_type = 'group_id'
-								AND audience_value IN(
-									SELECT a.group_id FROM `groups` AS a
-									JOIN `group_members` AS b
-									ON b.`group_id` = a.`group_id`
-									JOIN `group_organisations` AS c
-									ON c.`group_id` = a.`group_id`
-									WHERE b.`proxy_id` = ".$db->qstr($proxy_id)."
-									AND (b.`start_date` = 0
-									OR b.`start_date` <= UNIX_TIMESTAMP())
-									AND (b.`finish_date` = 0 OR b.`finish_date` >= UNIX_TIMESTAMP())
-									AND b.`member_active` = '1'
-									AND c.`organisation_id` = ".$db->qstr($ENTRADA_USER->getActiveOrganisation())."
-								)
-							)
-							OR (
-								audience_type='proxy_id'
-								AND audience_value = ".$db->qstr($proxy_id)."
-							)
-						)
-						AND audience_active = '1'
-					)";
-
-
-		$course_list = $db->CacheGetAll(CACHE_TIMEOUT, $query);
-		if ($course_list) {
-			foreach ($course_list as $course) {
-				$course_ids[] = (int) $course["course_id"];
-			}
-		}
-	}
-	
-	return $course_ids;
-}
-
-/**
  * This function returns the groups the enrolled in by a given proxy_id
  *
  * @param int $proxy_id

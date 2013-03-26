d8ab838e677e9ab8a3dda1a050e8297fc44a2552
Matt Simpson <simpson@queensu.ca>
2013-03-26 12:24:14 -0400

    Added missing </div> on communities create page.

--- www-root/core/modules/public/communities/create.inc.php
+++ www-root/core/modules/public/communities/create.inc.php
@@ -710,95 +710,96 @@ if ((!defined("PARENT_INCLUDED")) || (!defined("IN_COMMUNITIES"))) {
                 ?>
                 <div id="type_options"<?php echo (!isset($PROCESSED["octype_id"]) || !$PROCESSED["octype_id"] ? " style=\"display: none;\"" : ""); ?>>
 
-                <div class="control-group">
-                    <label class="control-label form-nrequired">Community Template:</label>
-                    <div class="controls">
-                        <?php
-                        if (isset($PROCESSED["octype_id"]) && $PROCESSED["octype_id"]) {
-                            $query = "SELECT a.* FROM `community_templates` AS a
-                                        JOIN `community_type_templates` AS b
-                                        ON a.`template_id` = b.`template_id`
-                                        WHERE b.`type_id` = ".$db->qstr($PROCESSED["octype_id"])."
-                                        AND b.`type_scope` = 'organisation'";
-                            $results = $db->GetAll($query);
-                            if ($results) {
-                                ?>
-                                <ul class="community-themes">
-                                    <?php
-                                    $default_templates = array();
-                                    $groups = array();
-                                    $category = array();
-                                    $default_categories = array();
-                                    $default_groups = array();
-                                    foreach ($results as $community_template) {
-                                        $permissions_query = "SELECT * FROM `communities_template_permissions` WHERE `template`=". $db->qstr($community_template["template_name"]);
-                                        $template_permissions = $db->GetAll($permissions_query);
-                                        if ($template_permissions) {
-                                            foreach ($template_permissions as $template_permission) {
-                                                if ($template_permission["permission_type"] == "group") {
-                                                    $groups = explode(",",$template_permission["permission_value"]);
-                                                }
-                                                if (($template_permission["permission_type"] == null && $template_permission["permission_value"] == null)) {
-                                                    $default_templates[] = $template_permission["template"];
-                                                }
-                                                if (($template_permission["permission_type"] == "category_id" && $template_permission["permission_value"] != null)) {
-                                                    $category = explode(",",$template_permission["permission_value"]);
-                                                }
-                                                if (($template_permission["permission_type"] == "category_id" && $template_permission["permission_value"] == null)) {
-                                                    $category_permissions_query = " SELECT * FROM `communities_template_permissions`
-                                                                                    WHERE `permission_type`= 'group'
-                                                                                    AND `template`=". $db->qstr($template_permission["template"]);
-                                                    $category_permissions = $db->GetAll($category_permissions_query);
-                                                    if ($category_permissions) {
-                                                        foreach ($category_permissions as $category_permission) {
-                                                            $default_categories = explode(",", $category_permission["permission_value"]);
-                                                            if (in_array($GROUP, $default_categories)) {
-                                                                $default_categories[] = $category_permission["template"];
-                                                            }
+                    <div class="control-group">
+                        <label class="control-label form-nrequired">Community Template:</label>
+                        <div class="controls">
+                            <?php
+                            if (isset($PROCESSED["octype_id"]) && $PROCESSED["octype_id"]) {
+                                $query = "SELECT a.* FROM `community_templates` AS a
+                                            JOIN `community_type_templates` AS b
+                                            ON a.`template_id` = b.`template_id`
+                                            WHERE b.`type_id` = ".$db->qstr($PROCESSED["octype_id"])."
+                                            AND b.`type_scope` = 'organisation'";
+                                $results = $db->GetAll($query);
+                                if ($results) {
+                                    ?>
+                                    <ul class="community-themes">
+                                        <?php
+                                        $default_templates = array();
+                                        $groups = array();
+                                        $category = array();
+                                        $default_categories = array();
+                                        $default_groups = array();
+                                        foreach ($results as $community_template) {
+                                            $permissions_query = "SELECT * FROM `communities_template_permissions` WHERE `template`=". $db->qstr($community_template["template_name"]);
+                                            $template_permissions = $db->GetAll($permissions_query);
+                                            if ($template_permissions) {
+                                                foreach ($template_permissions as $template_permission) {
+                                                    if ($template_permission["permission_type"] == "group") {
+                                                        $groups = explode(",",$template_permission["permission_value"]);
+                                                    }
+                                                    if (($template_permission["permission_type"] == null && $template_permission["permission_value"] == null)) {
+                                                        $default_templates[] = $template_permission["template"];
+                                                    }
+                                                    if (($template_permission["permission_type"] == "category_id" && $template_permission["permission_value"] != null)) {
+                                                        $category = explode(",",$template_permission["permission_value"]);
+                                                    }
+                                                    if (($template_permission["permission_type"] == "category_id" && $template_permission["permission_value"] == null)) {
+                                                        $category_permissions_query = " SELECT * FROM `communities_template_permissions`
+                                                                                        WHERE `permission_type`= 'group'
+                                                                                        AND `template`=". $db->qstr($template_permission["template"]);
+                                                        $category_permissions = $db->GetAll($category_permissions_query);
+                                                        if ($category_permissions) {
+                                                            foreach ($category_permissions as $category_permission) {
+                                                                $default_categories = explode(",", $category_permission["permission_value"]);
+                                                                if (in_array($GROUP, $default_categories)) {
+                                                                    $default_categories[] = $category_permission["template"];
+                                                                }
 
+                                                            }
                                                         }
                                                     }
                                                 }
-                                            }
-                                            if ((in_array($GROUP, $groups) && in_array($CATEGORY_ID, $category)) || (in_array($community_template["template_name"], $default_templates)) || (in_array($community_template["template_name"], $default_categories))) {
-                                            ?>
-                                            <li id="<?php echo $community_template["template_name"]."-template"; ?>">
-                                                <div class="template-rdo">
-                                                    <input type="radio" id="<?php echo "template_option_".$community_template["template_id"] ?>" name="template_selection" value="<?php echo $community_template["template_id"]; ?>"<?php echo ((($template_selection == 0) && ($community_template["template_id"] == 1) || ($template_selection == $community_template["template_id"])) ? " checked=\"checked\"" : ""); ?> />
-                                                </div>
-                                                <div class="large-view">
-                                                    <a href="#" class="<?php echo "large-view-".$community_template["template_id"]; ?>"><img src="<?php echo ENTRADA_URL. "/images/icon-magnify.gif"  ?>" /></a>
-                                                </div>
-                                                <label for="<?php echo "template_option_".$community_template["template_id"]; ?>"><?php echo ucfirst($community_template["template_name"]. " Template"); ?></label>
-                                            </li>
-                                            <?php
+                                                if ((in_array($GROUP, $groups) && in_array($CATEGORY_ID, $category)) || (in_array($community_template["template_name"], $default_templates)) || (in_array($community_template["template_name"], $default_categories))) {
+                                                ?>
+                                                <li id="<?php echo $community_template["template_name"]."-template"; ?>">
+                                                    <div class="template-rdo">
+                                                        <input type="radio" id="<?php echo "template_option_".$community_template["template_id"] ?>" name="template_selection" value="<?php echo $community_template["template_id"]; ?>"<?php echo ((($template_selection == 0) && ($community_template["template_id"] == 1) || ($template_selection == $community_template["template_id"])) ? " checked=\"checked\"" : ""); ?> />
+                                                    </div>
+                                                    <div class="large-view">
+                                                        <a href="#" class="<?php echo "large-view-".$community_template["template_id"]; ?>"><img src="<?php echo ENTRADA_URL. "/images/icon-magnify.gif"  ?>" /></a>
+                                                    </div>
+                                                    <label for="<?php echo "template_option_".$community_template["template_id"]; ?>"><?php echo ucfirst($community_template["template_name"]. " Template"); ?></label>
+                                                </li>
+                                                <?php
+                                                }
                                             }
                                         }
-                                    }
-                                    ?>
-                                </ul>
-                                <div class="default-large" style="display:none;">
-                                    <img src="<?php echo ENTRADA_URL."/images/template-default-large.gif" ?>" alt="Default Template Screen shot" />
-                                </div>
-                                <div class="committee-large" style="display:none;">
-                                    <img src="<?php echo ENTRADA_URL."/images/template-meeting-large.gif" ?>" alt="Committee Template Screen shot" />
-                                </div>
-                                <div class="vp-large" style="display:none;">
-                                    <img src="<?php echo ENTRADA_URL."/images/template-vp-large.gif" ?>" alt="Virtual Patient Template Screen shot" />
-                                </div>
-                                <div class="learningModule-large" style="display:none;">
-                                    <img src="<?php echo ENTRADA_URL."/images/template-education-large.gif" ?>" alt="Learning Template Screen shot" />
-                                </div>
-                                <div class="course-large" style="display:none;">
-                                    <img src="<?php echo ENTRADA_URL."/images/template-course-large.gif" ?>" alt="Course Template Screen shot" />
-                                </div>
-                                <?php
+                                        ?>
+                                    </ul>
+                                    <div class="default-large" style="display:none;">
+                                        <img src="<?php echo ENTRADA_URL."/images/template-default-large.gif" ?>" alt="Default Template Screen shot" />
+                                    </div>
+                                    <div class="committee-large" style="display:none;">
+                                        <img src="<?php echo ENTRADA_URL."/images/template-meeting-large.gif" ?>" alt="Committee Template Screen shot" />
+                                    </div>
+                                    <div class="vp-large" style="display:none;">
+                                        <img src="<?php echo ENTRADA_URL."/images/template-vp-large.gif" ?>" alt="Virtual Patient Template Screen shot" />
+                                    </div>
+                                    <div class="learningModule-large" style="display:none;">
+                                        <img src="<?php echo ENTRADA_URL."/images/template-education-large.gif" ?>" alt="Learning Template Screen shot" />
+                                    </div>
+                                    <div class="course-large" style="display:none;">
+                                        <img src="<?php echo ENTRADA_URL."/images/template-course-large.gif" ?>" alt="Course Template Screen shot" />
+                                    </div>
+                                    <?php
+                                }
                             }
-                        }
-                        ?>
+                            ?>
+                        </div>
                     </div>
                 </div>
-
+                        
                 <h3>Community Pages</h3>
 
                 <div class="control-group">

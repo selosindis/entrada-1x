98c7fb4c222b62622f396b860f8b43b576904445
James Ellis <james.ellis@queensu.ca>
2013-04-03 10:09:54 -0400

    Added in some baseline-required testing data for clerkship. Still needs events to be prepopulated for the clerks before any of the interfaces are available though.

--- www-root/setup/install/sql/entrada_clerkship.sql
+++ www-root/setup/install/sql/entrada_clerkship.sql
@@ -128,6 +128,21 @@ CREATE TABLE IF NOT EXISTS `categories` (
   KEY `category_status` (`category_status`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+INSERT INTO `categories` (`category_id`, `category_parent`, `category_code`, `category_type`, `category_name`, `category_desc`, `category_min`, `category_max`, `category_buffer`, `category_start`, `category_finish`, `subcategory_strict`, `category_expiry`, `category_status`, `category_order`, `rotation_id`)
+VALUES
+(1, 0, NULL, 12, 'School of Medicine', NULL, NULL, NULL, NULL, 0, 0, 0, 0, 'published', 0, 0),
+(2, 0, NULL, 13, 'All Students', NULL, NULL, NULL, NULL, 0, 1924927200, 0, 0, 'published', 0, 0)
+(3, 2, NULL, 17, 'Example Stream', NULL, 9, 9, NULL, 0, 1924927200, 0, 0, 'published', 0, 0),
+(4, 3, NULL, 32, 'Pediatrics', NULL, 9, 9, NULL, 0, 1924927200, 0, 0, 'published', 4, 4),
+(5, 3, NULL, 32, 'Obstetrics & Gynecology', NULL, 9, 9, NULL, 0, 1924927200, 0, 0, 'published', 3, 3),
+(6, 3, NULL, 32, 'Perioperative', NULL, 9, 9, NULL, 0, 1924927200, 0, 0, 'published', 5, 5),
+(7, 3, NULL, 32, 'Surgery - Urology', NULL, 9, 9, NULL, 0, 1924927200, 0, 0, 'published', 7, 7),
+(8, 3, NULL, 32, 'Surgery - Orthopedic', NULL, 9, 9, NULL, 0, 1924927200, 0, 0, 'published', 8, 8),
+(9, 3, NULL, 32, 'Family Medicine', NULL, 9, 9, NULL, 0, 1924927200, 0, 0, 'published', 1, 1),
+(10, 3, NULL, 32, 'Psychiatry', NULL, 9, 9, NULL, 0, 1924927200, 0, 0, 'published', 6, 6),
+(11, 3, NULL, 32, 'Medicine', NULL, 9, 9, NULL, 0, 1924927200, 0, 0, 'published', 2, 2),
+(12, 3, NULL, 32, 'Integrated', NULL, 2, 2, NULL, 0, 1924927200, 0, 0, 'published', 9, 9);
+
 CREATE TABLE IF NOT EXISTS `category_departments` (
   `cdep_id` int(12) NOT NULL AUTO_INCREMENT,
   `category_id` int(12) NOT NULL DEFAULT '0',
---
+++ www-root/setup/upgrade/1501/entrada.sql
@@ -0,0 +1,1 @@
+UPDATE `settings` SET `value` = '1501' WHERE `shortname` = 'version_db';

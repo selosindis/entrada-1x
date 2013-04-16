c834d910df8824200c32f2d855a045165bbe3c48
Andrew Dos-Santos <andrew.dos-santos@queensu.ca>
2013-04-16 14:03:09 -0400

    SQL Changes to allow for publications to be tied to profiles and departmental websites. #739

--- www-root/setup/upgrade/1500/entrada.sql
+++ www-root/setup/upgrade/1500/entrada.sql
@@ -2,5 +2,17 @@ INSERT INTO `quizzes_lu_questiontypes` (`questiontype_id`, `questiontype_title`,
 (2, 'Descriptive Text', '', 1, 0),
 (3, 'Page Break', '', 1, 0);
 
+ALTER TABLE `ar_peer_reviewed_papers` ADD `visible_on_website` INT(1)  NULL  DEFAULT '0'  AFTER `proxy_id`;
+
+ALTER TABLE `ar_non_peer_reviewed_papers` ADD `visible_on_website` INT(1)  NULL  DEFAULT '0'  AFTER `proxy_id`;
+
+ALTER TABLE `ar_book_chapter_mono` ADD `visible_on_website` INT(1)  NULL  DEFAULT '0'  AFTER `proxy_id`;
+
+ALTER TABLE `ar_poster_reports` ADD `visible_on_website` INT(1)  NULL  DEFAULT '0'  AFTER `proxy_id`;
+
+ALTER TABLE `ar_conference_papers` ADD `visible_on_website` INT(1)  NULL  DEFAULT '0'  AFTER `proxy_id`;
+
+ALTER TABLE `user_departments` ADD `entrada_only` INT(1)  NULL  DEFAULT '0'  AFTER `dep_title`;
+
 UPDATE `settings` SET `value` = '1500' WHERE `shortname` = 'version_db';
 UPDATE `settings` SET `value` = '1.5.0' WHERE `shortname` = 'version_entrada';

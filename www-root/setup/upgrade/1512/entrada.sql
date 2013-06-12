387a88a70bae350ebc98ad381e5b920305ee5a38
Andrew Dos-Santos <andrew.dos-santos@queensu.ca>
2013-06-12 14:35:15 -0400

    Moved 1512 update SQL to 1500

--- www-root/setup/upgrade/1512/entrada.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `draft_options` (
-  `draft_id` int(11) NOT NULL,
-  `option` varchar(255) NOT NULL DEFAULT '',
-  `value` varchar(30) NOT NULL DEFAULT ''
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-UPDATE `settings` SET `value` = '1512' WHERE `shortname` = 'version_db';

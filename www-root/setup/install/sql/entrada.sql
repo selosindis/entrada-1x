4379d7148943a7286ffb27bb5b6c512cdbb5bdc6
Josh Dillon <jdillon@queensu.ca>
2013-07-31 14:51:57 -0400

    Added objectives_release_date column to draft_events table

--- www-root/setup/install/sql/entrada.sql
+++ www-root/setup/install/sql/entrada.sql
@@ -2913,6 +2913,7 @@ CREATE TABLE IF NOT EXISTS `draft_events` (
   `include_parent_description` tinyint(1) NOT NULL DEFAULT '1',
   `event_goals` text,
   `event_objectives` text,
+  `objectives_release_date` bigint(64) DEFAULT '0',
   `event_message` text,
   `include_parent_message` tinyint(1) NOT NULL DEFAULT '1',
   `event_location` varchar(64) DEFAULT NULL,

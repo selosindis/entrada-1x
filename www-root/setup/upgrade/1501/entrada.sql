5ee029e29eccdb7521bf8d70dda8d4fe68eba007
Matt Simpson <simpson@queensu.ca>
2013-09-23 23:21:19 -0400

    Removed version information from non-versioned files.

--- www-root/setup/upgrade/1501/entrada.sql
+++ www-root/setup/upgrade/1501/entrada.sql
@@ -2,4 +2,3 @@ ALTER TABLE `events_lu_resources` ADD `organisation_id` INT(11)  NOT NULL  DEFAU
 ALTER TABLE `events_lu_resources` ADD `active` TINYINT(1)  NOT NULL  DEFAULT '1'  AFTER `updated_by`;
 
 UPDATE `settings` SET `value` = '1501' WHERE `shortname` = 'version_db';
-UPDATE `settings` SET `value` = '1.5.0.1' WHERE `shortname` = 'version_entrada';

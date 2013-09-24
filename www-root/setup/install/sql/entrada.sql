de89db0c1b0b522203a893e37ae2dccaee582b75
Matt Simpson <simpson@queensu.ca>
2013-09-24 14:18:58 -0400

    Added LTI database tables to install and upgrade SQL for Entrada 1.6.0.

--- www-root/setup/install/sql/entrada.sql
+++ www-root/setup/install/sql/entrada.sql
@@ -2105,7 +2105,6 @@ CREATE TABLE IF NOT EXISTS `course_group_contacts` (
   KEY `proxy_id` (`proxy_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-
 CREATE TABLE IF NOT EXISTS `course_links` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `course_id` int(12) NOT NULL DEFAULT '0',
@@ -2126,6 +2125,22 @@ CREATE TABLE IF NOT EXISTS `course_links` (
   KEY `valid_from` (`valid_from`),
   KEY `valid_until` (`valid_until`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `course_lti_consumers` (
+  `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+  `course_id` int(12) NOT NULL,
+  `is_required` int(1) NOT NULL DEFAULT '0',
+  `valid_from` bigint(64) NOT NULL,
+  `valid_until` bigint(64) NOT NULL,
+  `launch_url` text NOT NULL,
+  `lti_key` varchar(300) NOT NULL,
+  `lti_secret` varchar(300) NOT NULL,
+  `lti_title` varchar(300) NOT NULL,
+  `lti_notes` text NOT NULL,
+  `updated_date` bigint(64) NOT NULL,
+  `updated_by` int(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `course_objectives` (
   `cobjective_id` int(12) NOT NULL AUTO_INCREMENT,
@@ -2797,6 +2812,24 @@ CREATE TABLE IF NOT EXISTS `event_links` (
   KEY `required` (`required`),
   KEY `release_date` (`release_date`,`release_until`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `event_lti_consumers` (
+  `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+  `event_id` int(12) NOT NULL,
+  `is_required` int(1) NOT NULL,
+  `valid_from` bigint(64) NOT NULL,
+  `valid_until` bigint(64) NOT NULL,
+  `timeframe` varchar(64) NOT NULL,
+  `launch_url` text NOT NULL,
+  `lti_key` varchar(300) NOT NULL,
+  `lti_secret` varchar(300) NOT NULL,
+  `lti_title` varchar(300) NOT NULL,
+  `lti_notes` text NOT NULL,
+  `lti_params` text NOT NULL,
+  `updated_date` bigint(64) NOT NULL,
+  `updated_by` int(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `event_objectives` (
   `eobjective_id` int(12) NOT NULL AUTO_INCREMENT,
---
+++ www-root/setup/upgrade/1600/entrada.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS `event_lti_consumers` (
+  `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+  `event_id` int(12) NOT NULL,
+  `is_required` int(1) NOT NULL,
+  `valid_from` bigint(64) NOT NULL,
+  `valid_until` bigint(64) NOT NULL,
+  `timeframe` varchar(64) NOT NULL,
+  `launch_url` text NOT NULL,
+  `lti_key` varchar(300) NOT NULL,
+  `lti_secret` varchar(300) NOT NULL,
+  `lti_title` varchar(300) NOT NULL,
+  `lti_notes` text NOT NULL,
+  `lti_params` text NOT NULL,
+  `updated_date` bigint(64) NOT NULL,
+  `updated_by` int(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `course_lti_consumers` (
+  `id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+  `course_id` int(12) NOT NULL,
+  `is_required` int(1) NOT NULL DEFAULT '0',
+  `valid_from` bigint(64) NOT NULL,
+  `valid_until` bigint(64) NOT NULL,
+  `launch_url` text NOT NULL,
+  `lti_key` varchar(300) NOT NULL,
+  `lti_secret` varchar(300) NOT NULL,
+  `lti_title` varchar(300) NOT NULL,
+  `lti_notes` text NOT NULL,
+  `updated_date` bigint(64) NOT NULL,
+  `updated_by` int(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+UPDATE `settings` SET `value` = '1600' WHERE `shortname` = 'version_db';
+UPDATE `settings` SET `value` = '1.6.0' WHERE `shortname` = 'version_entrada';

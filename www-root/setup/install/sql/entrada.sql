516cf0be72e67a37a31b24b9e16cb78e11cf14ca
Matt Simpson <simpson@queensu.ca>
2013-09-26 22:32:33 -0400

    Removed the default hot topics from the Entrada installer.

--- www-root/setup/install/sql/entrada.sql
+++ www-root/setup/install/sql/entrada.sql
@@ -2577,47 +2577,6 @@ CREATE TABLE IF NOT EXISTS `events_lu_topics` (
   PRIMARY KEY (`topic_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO `events_lu_topics` (`topic_id`, `topic_name`, `topic_description`, `topic_type`, `updated_date`, `updated_by`) VALUES
-(1, 'Biostatistics', 'Biostatistics', 'ed10', 1215615910, 1),
-(2, 'Communication Skills', 'Communication Skills', 'ed10', 1215615910, 1),
-(3, 'Community Health', 'Community Health', 'ed10', 1215615910, 1),
-(4, 'End-of-Life Care', 'End-of-Life Care', 'ed10', 1215615910, 1),
-(5, 'Epidemiology', 'Epidemiology', 'ed10', 1215615910, 1),
-(6, 'Evidence-Based Medicine', 'Evidence-Based Medicine', 'ed10', 1215615910, 1),
-(7, 'Family Violence/Abuse', 'Family Violence/Abuse', 'ed10', 1215615910, 1),
-(8, 'Medical Genetics', 'Medical Genetics', 'ed10', 1215615910, 1),
-(9, 'Health Care Financing', 'Health Care Financing', 'ed10', 1215615910, 1),
-(10, 'Health Care Systems', 'Health Care Systems', 'ed10', 1215615910, 1),
-(11, 'Health Care Quality Review', 'Health Care Quality Review', 'ed10', 1215615910, 1),
-(12, 'Home Health Care', 'Home Health Care', 'ed10', 1215615910, 1),
-(13, 'Human Development/Life Cycle', 'Human Development/Life Cycle', 'ed10', 1215615910, 1),
-(14, 'Human Sexuality', 'Human Sexuality', 'ed10', 1215615910, 1),
-(15, 'Medical Ethics', 'Medical Ethics', 'ed10', 1215615910, 1),
-(16, 'Medical Humanities', 'Medical Humanities', 'ed10', 1215615910, 1),
-(17, 'Medical Informatics', 'Medical Informatics', 'ed10', 1215615910, 1),
-(18, 'Medical Jurisprudence', 'Medical Jurisprudence', 'ed10', 1215615910, 1),
-(19, 'Multicultural Medicine', 'Multicultural Medicine', 'ed10', 1215615910, 1),
-(20, 'Nutrition', 'Nutrition', 'ed10', 1215615910, 1),
-(21, 'Occupational Health/Medicine', 'Occupational Health/Medicine', 'ed10', 1215615910, 1),
-(22, 'Pain Management', 'Pain Management', 'ed10', 1215615910, 1),
-(23, 'Palliative Care', 'Palliative Care', 'ed10', 1215615910, 1),
-(24, 'Patient Health Education', 'Patient Health Education', 'ed10', 1215615910, 1),
-(25, 'Population-Based Medicine', 'Population-Based Medicine', 'ed10', 1215615910, 1),
-(26, 'Practice Management', 'Practice Management', 'ed10', 1215615910, 1),
-(27, 'Preventive Medicine', 'Preventive Medicine', 'ed10', 1215615910, 1),
-(28, 'Rehabilitation/Care of the Disabled', 'Rehabilitation/Care of the Disabled', 'ed10', 1215615910, 1),
-(29, 'Research Methods', 'Research Methods', 'ed10', 1215615910, 1),
-(30, 'Substance Abuse', 'Substance Abuse', 'ed10', 1215615910, 1),
-(31, 'Womens Health', 'Womens Health', 'ed10', 1215615910, 1),
-(32, 'Anatomy', 'Anatomy', 'ed11', 1215615910, 1),
-(33, 'Biochemistry', 'Biochemistry', 'ed11', 1215615910, 1),
-(34, 'Genetics', 'Genetics', 'ed11', 1215615910, 1),
-(35, 'Physiology', 'Physiology', 'ed11', 1215615910, 1),
-(36, 'Microbiology and Immunology', 'Microbiology and Immunology', 'ed11', 1215615910, 1),
-(37, 'Pathology', 'Pathology', 'ed11', 1215615910, 1),
-(38, 'Pharmacology Therapeutics', 'Pharmacology Therapeutics', 'ed11', 1215615910, 1),
-(39, 'Preventive Medicine', 'Preventive Medicine', 'ed11', 1215615910, 1);
-
 CREATE TABLE IF NOT EXISTS `events_lu_eventtypes` (
   `eventtype_id` int(12) NOT NULL AUTO_INCREMENT,
   `eventtype_title` varchar(32) NOT NULL,
@@ -4823,8 +4782,6 @@ CREATE TABLE IF NOT EXISTS `topic_organisation`(
   `organisation_id` INT(12) NOT NULL,
   PRIMARY KEY(`topic_id`,`organisation_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-INSERT INTO `topic_organisation` SELECT `topic_id`, 1 FROM `events_lu_topics`;
 
 CREATE TABLE IF NOT EXISTS `curriculum_lu_levels` (
   `curriculum_level_id` int(11) unsigned NOT NULL auto_increment,

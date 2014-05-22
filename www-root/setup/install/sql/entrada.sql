8ce3ed752e75513e6ad2a67ad184f32b0599c0c9
Josh Dillon <jdillon@queensu.ca>
2014-05-22 13:37:22 -0400

    adds assessment_events table to the install sql file. Refs #392

--- www-root/setup/install/sql/entrada.sql
+++ www-root/setup/install/sql/entrada.sql
@@ -5305,3 +5305,13 @@ CREATE TABLE IF NOT EXISTS `evaluation_progress_patient_encounters` (
   `encounter_complexity` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`eppencounter_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `assessment_events` (
+  `assessment_event_id` int(12) unsigned NOT NULL AUTO_INCREMENT,
+  `assessment_id` int(12) unsigned NOT NULL,
+  `event_id` int(12) unsigned NOT NULL,
+  `updated_date` bigint(64) NOT NULL DEFAULT '0',
+  `updated_by` int(12) unsigned NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`assessment_event_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;

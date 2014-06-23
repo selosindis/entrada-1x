e24ad7cadb30d0bec807df03ed21f74724508658
Robert Fotino <robert.fotino@gmail.com>
2014-06-23 14:48:19 -0700

    Moved multiple assignment files sql upgrade to new db version folder.

--- www-root/setup/upgrade/1627/entrada.sql
+++ www-root/setup/upgrade/1627/entrada.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `assignments` ADD `max_file_uploads` INT(11)  NOT NULL DEFAULT 1 AFTER `assignment_uploads`;
 ALTER TABLE `assignment_comments` DROP COLUMN `afile_id`;
 
-UPDATE `settings` SET `value` = '1607' WHERE `shortname` = 'version_db';
+UPDATE `settings` SET `value` = '1627' WHERE `shortname` = 'version_db';
